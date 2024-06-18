b7fa64e32f9626714a7e2cccb887dcd7bd3e7218
hamzabourass <roxmax224@gmail.com>
2024-06-18 23:46:01 +0100

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -25,16 +25,16 @@ fi
 
 if [ "$ENVIRONMENT" = "staging" ]; then
   echo "Deploying to staging environment..."
-  scp -o StrictHostKeyChecking=no -i "${SSH_PRIVATE_KEY_FILE}" docker-compose.yml ubuntu@ec2-100-25-16-179.compute-1.amazonaws.com:/home/ubuntu/docker-compose.yml
-  ssh -o StrictHostKeyChecking=no -i "${SSH_PRIVATE_KEY_FILE}" ubuntu@ec2-100-25-16-179.compute-1.amazonaws.com 'cd /home/ubuntu && docker-compose up -d'
+  scp -o StrictHostKeyChecking=no -i "${SSH_PRIVATE_KEY_FILE}" docker-compose.yml ubuntu@ec2-100-25-16-179.compute-1.amazonaws.com:/home/docker-compose.yml
+  ssh -o StrictHostKeyChecking=no -i "${SSH_PRIVATE_KEY_FILE}" ubuntu@ec2-100-25-16-179.compute-1.amazonaws.com 'cd /home && docker-compose up -d'
 elif [ "$ENVIRONMENT" = "production" ]; then
   echo "Deploying to production environment..."
-  scp -o StrictHostKeyChecking=no -i "${SSH_PRIVATE_KEY_FILE}" docker-compose.yml ec2-user@ec2-54-224-29-112.compute-1.amazonaws.com:/home/ec2-user/docker-compose.yml
-  ssh -o StrictHostKeyChecking=no -i "${SSH_PRIVATE_KEY_FILE}" ec2-user@<production-ec2-instance-public-dns> 'cd /home/ec2-user && docker-compose up -d'
+  scp -o StrictHostKeyChecking=no -i "${SSH_PRIVATE_KEY_FILE}" docker-compose.yml ec2-user@ec2-54-224-29-112.compute-1.amazonaws.com:/home/docker-compose.yml
+  ssh -o StrictHostKeyChecking=no -i "${SSH_PRIVATE_KEY_FILE}" ec2-user@<production-ec2-instance-public-dns> 'cd /home && docker-compose up -d'
 elif [ "$ENVIRONMENT" = "development" ]; then
   echo "Deploying to development environment..."
-  scp -o StrictHostKeyChecking=no -i "${SSH_PRIVATE_KEY_FILE}" docker-compose.yml ec2-user@ec2-54-224-29-112.compute-1.amazonaws.com:/home/ec2-user/docker-compose.yml
-  ssh -o StrictHostKeyChecking=no -i "${SSH_PRIVATE_KEY_FILE}" ec2-user@<development-ec2-instance-public-dns> 'cd /home/ec2-user && docker-compose up -d'
+  scp -o StrictHostKeyChecking=no -i "${SSH_PRIVATE_KEY_FILE}" docker-compose.yml ec2-user@ec2-54-224-29-112.compute-1.amazonaws.com:/home/docker-compose.yml
+  ssh -o StrictHostKeyChecking=no -i "${SSH_PRIVATE_KEY_FILE}" ec2-user@<development-ec2-instance-public-dns> 'cd /home && docker-compose up -d'
 else
   echo "Unknown environment: $ENVIRONMENT"
   exit 1
