beb20af057d25e7f3a395f59f133ec8cc1ab1290
hamzabourass <roxmax224@gmail.com>
2024-06-19 00:15:05 +0100

    test to deploy app to ec2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -38,7 +38,7 @@ fi
 if [ "$ENVIRONMENT" = "staging" ]; then
   echo "Deploying to staging environment..."
   scp -o StrictHostKeyChecking=no -i "${SSH_PRIVATE_KEY_FILE}" ./scripts/docker-compose.yml ubuntu@ec2-100-25-16-179.compute-1.amazonaws.com:/home/ubuntu/docker-compose.yml
-  ssh -o StrictHostKeyChecking=no -i "${SSH_PRIVATE_KEY_FILE}" ubuntu@ec2-100-25-16-179.compute-1.amazonaws.com  'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 714714593268.dkr.ecr.us-east-1.amazonaws.com && cd /home/ubuntu && docker-compose up -d'
+  ssh -o StrictHostKeyChecking=no -i "${SSH_PRIVATE_KEY_FILE}" ubuntu@ec2-100-25-16-179.compute-1.amazonaws.com  'sudo apt-get update && sudo apt-get install -y awscli && aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 714714593268.dkr.ecr.us-east-1.amazonaws.com && cd /home/ubuntu && docker-compose up -d'
 elif [ "$ENVIRONMENT" = "production" ]; then
   echo "Deploying to production environment..."
   scp -o StrictHostKeyChecking=no -i "${SSH_PRIVATE_KEY_FILE}" docker-compose.yml ec2-user@ec2-54-224-29-112.compute-1.amazonaws.com:/home/docker-compose.yml
